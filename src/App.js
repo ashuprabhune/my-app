92894baed5168fbc4e83892a060b5dc3f28cdae4
Ashish Prabhune <ashuprabhune@gmail.com>
2020-08-20 14:11:29 -0400

    Demostrating the use of state in class Components.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person'
@@ -11,8 +11,7 @@ function App() {
         <p>
           Hi I am react's hello world.
         </p>
-        <Person name="Ashish" age="25"/>
-        <Person name="Girish" age="25"> I am Single </Person>
+
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -21,9 +20,37 @@ function App() {
         >
           Learn React
         </a>
+        <NewComponent/>
       </header>
     </div>
   );
 }
 
+
+class NewComponent extends Component{
+  state = {
+    persons:[
+      {name : 'Ashish', age : "25"},
+      {name : 'Girish', age : "26"}
+    ]
+  }
+  switchNameHandler = () => {
+    this.setState({
+      persons:[
+        {name : 'Ashish Prabhune', age : "30"},
+        {name : 'Girish', age : "26"}
+      ]
+    });
+  }
+
+  render(){
+    return(
+      <div>
+      <button onClick = {this.switchNameHandler}>Swtich Name</button>
+      <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+      <Person name={this.state.persons[1].name} age={this.state.persons[1].age}> I am Single </Person>
+      </div>
+    )
+  }
+}
 export default App;
