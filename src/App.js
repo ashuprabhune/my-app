6301db2808b8a744a4430b932401268f1697660e
Ashish Prabhune <ashuprabhune@gmail.com>
2020-08-19 22:37:00 -0400

    Creating the custom Component in React.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person'
 
 function App() {
   return (
@@ -8,8 +9,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi I am react's hello world.
         </p>
+        <Person />
         <a
           className="App-link"
           href="https://reactjs.org"
