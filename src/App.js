bf6404610dad466e32a35c67a5c8a9a4f7c5d2e7
Ashish Prabhune <ashuprabhune@gmail.com>
2020-08-20 14:45:20 -0400

    Using useState hook for functional states

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import  { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person'
 
 function App() {
+  const [personsState, setPersonsState] = useState({
+    persons:[
+      {name : 'Ajit', age : "25"},
+      {name : 'Puki', age : "26"}
+    ],
+    otherState : "Hello"
+  })
+
+  const switchNameFuncHandler = () =>{
+    setPersonsState({
+      persons:[
+        {name : 'Ajit BusNaik', age : "25"},
+        {name : 'Puki Datar', age : "26"}
+      ]
+    })
+  }
+
+  console.log(personsState);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi I am react's hello world.
+          Hi I ' am react's hello world.
         </p>
-
+        <button onClick = {switchNameFuncHandler}>Swtich Name</button>
+        <div> This is from functional state</div>
+        <Person name = {personsState.persons[0].name} age = {personsState.persons[0].age} />
+        <Person name = {personsState.persons[1].name} age = {personsState.persons[1].age} />
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -46,6 +68,7 @@ class NewComponent extends Component{
   render(){
     return(
       <div>
+      <p> I'am </p>
       <button onClick = {this.switchNameHandler}>Swtich Name</button>
       <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
       <Person name={this.state.persons[1].name} age={this.state.persons[1].age}> I am Single </Person>
