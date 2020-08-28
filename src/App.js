4ddf1fe7133e1b90ba061be1540bca0ad0af7c7c
Ashish Prabhune <ashuprabhune@gmail.com>
2020-08-28 14:17:19 -0400

    Used styled-components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import  { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person'
+import styled from 'styled-components'
+
 
 function App() {
   const [personsState, setPersonsState] = useState({
@@ -62,58 +64,62 @@ function App() {
   );
 }
 
+const StyledButton = styled.button`
+      background-color: green;
+      color:white;
+      font:inherit;
+      border: 1px solid blue;
+      padding: 8px;
+      &:hover{
+        background-color: lightgreen;
+        color:black;
+      }
+`
 
 class NewComponent extends Component{
 
   state = {
     persons:[
-      {name : 'Ashish', age : "25"},
-      {name : 'Girish', age : "26"}
+      {id : "asdadasdasd",name : 'Ashish', age : "25"},
+      {id : "ewwrfd",name : 'Girish', age : "26"}
     ],
     showPersons : false
   }
-  switchNameHandler = () => {
-    this.setState({
-      persons:[
-        {name : 'Ashish Prabhune', age : "25"},
-        {name : 'Girish', age : "26"}
-      ]
-    });
-  }
-  nameChangedHandler = (event) => {
-    this.setState({
-      persons:[
-        {name : 'Ashish', age : "25"},
-        {name : event.target.value, age : "26"}
-      ]
-    });
+
+  nameChangedHandler = (event, id) => {
+    console.log("Changing name");
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id
+    })
+    console.log(id);
+    const newPerson = {
+      ...this.state.persons[personIndex]
+    }
+    newPerson.name = event.target.value
+    const newPersonState = [...this.state.persons]
+    newPersonState[personIndex] = newPerson
+    this.setState({ persons:newPersonState });
   }
 
   togglePersonsHandler = () =>{
     const showStatus = this.state.showPersons;
     this.setState(
       {showPersons : !showStatus}
-    )
+    );
   }
 
   deletePerson = (index) => {
-    const personCopy = this.state.persons.slice()
-    personCopy.splice(index,1)
+    const personCopy = this.state.persons.slice();
+    personCopy.splice(index,1);
     this.setState({
       persons : personCopy
-    })
+    });
     console.log(this.state.persons);
 
   }
 
 
   render(){
-    const style = {
-      backgroundColor: 'white',
-      font:'inherit',
-      border: '1px solid blue',
-      padding: '8px'
-    };
 
     let persons = null;
     if(this.state.showPersons){
@@ -121,24 +127,37 @@ class NewComponent extends Component{
         <div>
           {
             this.state.persons.map((person,index) => {
+              console.log(person.id);
               return <Person
-                key= {person.name}
+                key={person.id}
                 name={person.name}
                 age={person.age}
-                click= {() => this.deletePerson(index)}/>
+                click= {() => this.deletePerson(index)}
+                changename = {(event) => this.nameChangedHandler(event,person.id)}/>
             })
           }
         </div>
-      )
+      );
+
+    }
+
+    let classes = [];
+
+    if(this.state.persons.length <= 1){
+      classes.push('red');
+    }
+    if(this.state.persons.length == 0){
+      classes.push('bold');
     }
 
     return(
       <div>
-        <p> I'am </p>
-        <button style = {style} onClick = {this.togglePersonsHandler}>Toggle Button</button>
+        <p className={classes.join(' ')}> Hello from class component </p>
+      <StyledButton onClick = {this.togglePersonsHandler}>Toggle Button</StyledButton>
         {persons}
       </div>
-    )
+    );
   }
 }
+
 export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,11 +1,28 @@
 import React from "react"
 import './Person.css'
+import styled from 'styled-components'
+
+const StyledDiv = styled.div`
+  width: 60%;
+  margin: 16px auto;
+  border: 1px solid #eee;
+  box-shadow: 0 2px 3px #ccc;
+  padding: 16px;
+  text-align: center;
+
+  &:hover{
+    background-color: red;
+    color:black
+  }
+`
 const person = (props) => {
-  return (<div className="Person">
-          <p onClick={props.click}> I am {props.name}. My age is {props.age}</p>
+
+  return (
+        <StyledDiv>
+          <p  onClick={props.click}> I am {props.name}. My age is {props.age}</p>
           <p > {props.children}</p>
           <input type="text" onChange = {props.changename} value={props.name}/>
-          </div>
+        </StyledDiv>
         );
 
 }
